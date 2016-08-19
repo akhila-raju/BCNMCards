b219e77cd58b03efa7062328435fc7af27ddbc29
Akhila Raju <akhilaraju@berkeley.edu>
2016-08-19 15:30:25 -0700

    Added Rimplementation for R - random sort, N - sequential order, S - suit order

--- Assets/scripts/Rotation.cs
+++ Assets/scripts/Rotation.cs
@@ -33,30 +33,102 @@ public class Rotation : MonoBehaviour {
 	public GameObject card03;
 	private GameObject card;
 	private Vector3 vector;
-	private List<Tuple<Vector3, float>> unitCirclePos;
+	private List<Tuple<Vector3, float>> SuitOrderArray;
+	private List<Tuple<Vector3, float>> RandomOrderArray;
+	private Vector3[] SequentialOrderArray;
 
-	private bool randomlySorted;
-	private List<Tuple<Vector3, float>> shuffledCards;
+//	private bool randomlySorted;
 	private List<Tuple<Vector3, float>> shuffledCardsWithRemoval;
+
 
 	// Use this for initialization
 
 	void Start () {
 
-		// Initialize list to store positions and heights
-		unitCirclePos = new List<Tuple<Vector3, float>> ();
-		// Initialize variables used to calculate position.
-		float theta = 0;
-		float radius = (float)3;
-		float xPos = 3;
-		float height = 0;
-		float zPos = 0;
-		Vector3 xzPos = new Vector3 (xPos, height, zPos);
-		// Initialize variable used to store position and height
-		var posAndHeight = Tuple.New (xzPos, height);;
-		// Add first card's position and height to list
-		unitCirclePos.Add (posAndHeight);
+		// S INPUT: Order cards by suits
+		SuitOrderArray = new List<Tuple<Vector3, float>> ();
+		float theta;
+		float radius = 3f;
+		float xPos;
+		float height;
+		float zPos;
+		Vector3 xzPos;
+		// Initialize variable used to calculate xz position
+		float thetaStart = 0;
 
+		for (int i = 0; i < 4; i++) {
+
+
+			if (i == 0) {
+				thetaStart = 12;	
+			} else if (i == 1) {
+				thetaStart = 25;
+			} else if (i == 2) {
+				thetaStart = 38;
+			} else if (i == 3) {
+				thetaStart = 51;
+			}
+				
+			for (int j = 0; j < 13; j++) {
+
+				// height
+				if (j == 0) {
+					height = 2.5f;
+				} else if (j < 4) {
+					height = 1.25f;
+				} else if (j < 9) {
+					height = 0f;
+				} else if (j < 12) {
+					height = -1.25f;
+				} else {
+					height = -2.5f;
+				}
+
+				// Calculate angle around sphere
+				if (j == 1 | j == 5 | j == 9) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart-1f);
+				} else if (j == 4) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart-2f);
+				} else if (j == 3 | j == 7 | j == 11) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart+1f);
+				} else if (j == 8) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart+2f);
+				} else {
+					theta = (2 * Mathf.PI / 52) * thetaStart;
+				}
+
+				// Calculate x position
+				xPos = Mathf.Cos (theta) * radius;
+				// Calculate z position
+				zPos = Mathf.Sin (theta) * radius;
+				// Add position and height to list
+				xzPos = new Vector3 (xPos, 0, zPos);
+				// S INPUT : includes variable height
+				var suitPosAndHeight = Tuple.New(xzPos, height);
+				SuitOrderArray.Add (suitPosAndHeight);
+
+			}
+
+		}
+
+		// R INPUT: Initialize list to store positions and heights
+		RandomOrderArray = new List<Tuple<Vector3, float>> ();
+		// Initialize variables used to calculate position.
+		theta = 0;
+		radius = 3f;
+		xPos = 3;
+		height = 0;
+		zPos = 0;
+		xzPos = new Vector3 (xPos, height, zPos);
+		var posAndHeight = Tuple.New (xzPos, height);
+		// Add first card's position and height to list
+		RandomOrderArray.Add (posAndHeight);
+
+		// N INPUT : Initialize list to store positions
+		SequentialOrderArray = new Vector3[52];
+		SequentialOrderArray[0] = xzPos;
+
+		// CREATE ARRAYS FOR N AND R INPUTS, INITIALIZE CARDS WITH RANDOM ORDER AND HEIGHT
 		for (int i = 1; i < 52; i++) {
 
 			// Calculate angle around sphere
@@ -69,8 +141,12 @@ public class Rotation : MonoBehaviour {
 			zPos = Mathf.Sin (theta) * radius;
 			// Add position and height to list
 			xzPos = new Vector3 (xPos, 0, zPos);
+			// S INPUT : includes variable height
 			posAndHeight = Tuple.New(xzPos, height);
-			unitCirclePos.Add (posAndHeight);
+			RandomOrderArray.Add (posAndHeight);
+
+			// N INPUT : without height
+			SequentialOrderArray[i] = xzPos;
 
 
 			// CREATE CLONE AND ASSIGN TO JUST CALCULATED POSITION.
@@ -99,8 +175,7 @@ public class Rotation : MonoBehaviour {
 
 		}
 
-		randomlySorted = false;
-			
+//		randomlySorted = false;
 
 	}
 
@@ -108,113 +183,221 @@ public class Rotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyUp (KeyCode.R)) {
 
-			if (randomlySorted == false) {
 
-				// shuffle cards into random order -- working
-				shuffledCardsWithRemoval = unitCirclePos.OrderBy (a => System.Guid.NewGuid ()).ToList ();;
+		// Suit Order, at every 90 degrees
+		if (Input.GetKeyUp (KeyCode.S)) {
 
-				// find first card
-				GameObject currentClone = GameObject.Find (card00.name);
-				// get angle and height of first card
-				Vector3 xzPos = shuffledCardsWithRemoval [0].First;
-				float height = Random.Range(-2.5f, 2.5f);
-				shuffledCardsWithRemoval.RemoveAt (0);
-		
-				// Move position
-				currentClone.transform.position = xzPos;
-				float xPos = currentClone.transform.position.x;
-				float zPos = currentClone.transform.position.z;
-				iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
+			// find first card
+			GameObject currentClone = GameObject.Find (card00.name);
+			// get angle and height of first card
+			Vector3 xzPos = SuitOrderArray [0].First;
+			float height = SuitOrderArray [0].Second;
 
-				// Face camera
-				currentClone.transform.LookAt (Vector3.zero);
-				// Turn 180 so front of card is facing camera
-				currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
 
-				for (int i = 1; i < 52; i++) {
+			// Move position
+			currentClone.transform.position = xzPos;
+			float xPos = currentClone.transform.position.x;
+			float zPos = currentClone.transform.position.z;
+			iTween.MoveTo(currentClone, new Vector3(xPos, height, zPos), 5);
 
-					xzPos = shuffledCardsWithRemoval [0].First;
-					height = Random.Range(-2.5f, 2.5f);
-					shuffledCardsWithRemoval.RemoveAt (0);
+			// Face camera
+			currentClone.transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
 
-					if (i < 13) {
-						card = card00;
-					} else if (i < 26) {
-						card = card01;
-					} else if (i < 39) {
-						card = card02;
-					} else {
-						card = card03;
-					}
-					string cloneName = card.name + "_" + i.ToString ();
-					currentClone = GameObject.Find (cloneName);
+			for (int i = 1; i < 52; i++) {
 
-					// Move x and z positions
-					currentClone.transform.position = xzPos;
-					xPos = currentClone.transform.position.x;
-					zPos = currentClone.transform.position.z;
-					iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
-					// Face camera
-					currentClone.transform.LookAt (Vector3.zero);
-					// Turn 180 so front of card is facing camera
-					currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+				xzPos = SuitOrderArray [i].First;
+				height = SuitOrderArray [i].Second;
 
+				if (i < 13) {
+					card = card00;
+				} else if (i < 26) {
+					card = card01;
+				} else if (i < 39) {
+					card = card02;
+				} else {
+					card = card03;
 				}
-
-				randomlySorted = true;
-
-			} else {
-
-				// find first card
-				GameObject currentClone = GameObject.Find (card00.name);
-				// get angle and height of first card
-				Vector3 xzPos = unitCirclePos [0].First;
-				float height = unitCirclePos [0].Second;
+				string cloneName = card.name + "_" + i.ToString ();
+				currentClone = GameObject.Find (cloneName);
 
 				// Move x and z positions
 				currentClone.transform.position = xzPos;
-				float xPos = currentClone.transform.position.x;
-				float zPos = currentClone.transform.position.z;
+				xPos = currentClone.transform.position.x;
+				zPos = currentClone.transform.position.z;
+				iTween.MoveTo(currentClone, new Vector3(xPos, height, zPos), 5);
+				// Face camera
+				currentClone.transform.LookAt (Vector3.zero);
+				// Turn 180 so front of card is facing camera
+				currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+			}
+
+		}
+			
+		
+
+		// Sequential Order, Same Height
+		if (Input.GetKeyUp (KeyCode.N)) {
+
+			// find first card
+			GameObject currentClone = GameObject.Find (card00.name);
+			// get angle and height of first card
+			Vector3 xzPos = SequentialOrderArray [0];
+
+			// Move position
+			currentClone.transform.position = xzPos;
+			float xPos = currentClone.transform.position.x;
+			float zPos = currentClone.transform.position.z;
+			iTween.MoveTo(currentClone, new Vector3(xPos,0,zPos), 5);
+
+			// Face camera
+			currentClone.transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+			for (int i = 1; i < 52; i++) {
+
+				xzPos = SequentialOrderArray [i];
+
+				if (i < 13) {
+					card = card00;
+				} else if (i < 26) {
+					card = card01;
+				} else if (i < 39) {
+					card = card02;
+				} else {
+					card = card03;
+				}
+				string cloneName = card.name + "_" + i.ToString ();
+				currentClone = GameObject.Find (cloneName);
+
+				// Move x and z positions
+				currentClone.transform.position = xzPos;
+				xPos = currentClone.transform.position.x;
+				zPos = currentClone.transform.position.z;
+				iTween.MoveTo(currentClone, new Vector3(xPos,0,zPos), 5);
+				// Face camera
+				currentClone.transform.LookAt (Vector3.zero);
+				// Turn 180 so front of card is facing camera
+				currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+			}
+
+		}
+
+
+
+		// Random Order, Random Height
+		if (Input.GetKeyUp (KeyCode.R)) {
+
+//			if (randomlySorted == false) {
+
+			// shuffle cards into random order -- working
+			shuffledCardsWithRemoval = RandomOrderArray.OrderBy (a => System.Guid.NewGuid ()).ToList ();;
+
+			// find first card
+			GameObject currentClone = GameObject.Find (card00.name);
+			// get angle and height of first card
+			Vector3 xzPos = shuffledCardsWithRemoval [0].First;
+			float height = Random.Range(-2.5f, 2.5f);
+			shuffledCardsWithRemoval.RemoveAt (0);
+	
+			// Move position
+			currentClone.transform.position = xzPos;
+			float xPos = currentClone.transform.position.x;
+			float zPos = currentClone.transform.position.z;
+			iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
+
+			// Face camera
+			currentClone.transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+			for (int i = 1; i < 52; i++) {
+
+				xzPos = shuffledCardsWithRemoval [0].First;
+				height = Random.Range(-2.5f, 2.5f);
+				shuffledCardsWithRemoval.RemoveAt (0);
+
+				if (i < 13) {
+					card = card00;
+				} else if (i < 26) {
+					card = card01;
+				} else if (i < 39) {
+					card = card02;
+				} else {
+					card = card03;
+				}
+				string cloneName = card.name + "_" + i.ToString ();
+				currentClone = GameObject.Find (cloneName);
+
+				// Move x and z positions
+				currentClone.transform.position = xzPos;
+				xPos = currentClone.transform.position.x;
+				zPos = currentClone.transform.position.z;
 				iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
 				// Face camera
 				currentClone.transform.LookAt (Vector3.zero);
 				// Turn 180 so front of card is facing camera
 				currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
 
-				for (int i = 1; i < 52; i++) {
-
-					xzPos = unitCirclePos [i].First;
-					height = unitCirclePos [i].Second;
-
-					if (i < 13) {
-						card = card00;
-					} else if (i < 26) {
-						card = card01;
-					} else if (i < 39) {
-						card = card02;
-					} else {
-						card = card03;
-					}
-					string cloneName = card.name + "_" + i.ToString ();
-					currentClone = GameObject.Find (cloneName);
-
-					// Move x and z positions
-					currentClone.transform.position = xzPos;
-					xPos = currentClone.transform.position.x;
-					zPos = currentClone.transform.position.z;
-					iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
-					// Face camera
-					currentClone.transform.LookAt (Vector3.zero);
-					// Turn 180 so front of card is facing camera
-					currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
-
-				}
-					
-				randomlySorted = false;
-
 			}
+//
+//				randomlySorted = true;
+//
+//			} else {
+//
+//				// find first card
+//				GameObject currentClone = GameObject.Find (card00.name);
+//				// get angle and height of first card
+//				Vector3 xzPos = RandomOrderArray [0].First;
+//				float height = RandomOrderArray [0].Second;
+//
+//				// Move x and z positions
+//				currentClone.transform.position = xzPos;
+//				float xPos = currentClone.transform.position.x;
+//				float zPos = currentClone.transform.position.z;
+//				iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
+//				// Face camera
+//				currentClone.transform.LookAt (Vector3.zero);
+//				// Turn 180 so front of card is facing camera
+//				currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+//
+//				for (int i = 1; i < 52; i++) {
+//
+//					xzPos = RandomOrderArray [i].First;
+//					height = RandomOrderArray [i].Second;
+//
+//					if (i < 13) {
+//						card = card00;
+//					} else if (i < 26) {
+//						card = card01;
+//					} else if (i < 39) {
+//						card = card02;
+//					} else {
+//						card = card03;
+//					}
+//					string cloneName = card.name + "_" + i.ToString ();
+//					currentClone = GameObject.Find (cloneName);
+//
+//					// Move x and z positions
+//					currentClone.transform.position = xzPos;
+//					xPos = currentClone.transform.position.x;
+//					zPos = currentClone.transform.position.z;
+//					iTween.MoveTo(currentClone, new Vector3(xPos,height,zPos), 5);
+//					// Face camera
+//					currentClone.transform.LookAt (Vector3.zero);
+//					// Turn 180 so front of card is facing camera
+//					currentClone.transform.RotateAround (Vector3.zero, Vector3.up, 180);
+//
+//				}
+//					
+//				randomlySorted = false;
+//
+//			}
 
 		}
 
